831fe94de849162c590e124566c025c17536584f
fdecaire <frank@decaire.net>
2015-07-03 11:39:44 -0400

    initial check-in

---
+++ MVCModelDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCModelDemo.Models;
+
+namespace MVCModelDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+			var addressRecord = new AddressViewModel
+			{
+				Id = 5,
+				Address = "123 Main St.",
+				City = "Rockville",
+				State = "MD",
+				Zip = "12345"
+			};
+
+            return View(addressRecord);
+        }
+
+		[HttpPost]
+		public ActionResult Save(AddressViewModel addressRecord)
+		{
+			addressRecord.Save();
+			return View("Index");
+		}
+    }
+}
---
+++ MVCModelDemo/Models/AddressViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCModelDemo.Models
+{
+	public class AddressViewModel
+	{
+		public int Id { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+
+		public void Save()
+		{
+			// save this record
+		}
+	}
+}
